8bb3d602e628de97ad5d12e896d2910b2b9e9373
revansiddappapanchal <revansiddappapanchal@LAPTOP-FG948455>
2021-05-28 10:18:24 +0530

    my first project commit

---
+++ zoho_Github/src/test/java/com/zoho/Customer/CreateCustomerTest.java
@@ -0,0 +1,13 @@
+package com.zoho.Customer;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateCustomerTest {
+
+	@Test
+	public void run()
+	{
+		Reporter.log("Test Running Successfully",true);
+	}
+}
