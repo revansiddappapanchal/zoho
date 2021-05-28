e0ddb6ac1dac923bcd46d78f4ddf8cf268b09fdf
revansiddappapanchal <revansiddappapanchal@LAPTOP-FG948455>
2021-05-28 10:55:51 +0530

    create project commit

---
+++ zoho_Github/src/test/java/com/zoho/Customer/CreateProjectTest.java
@@ -0,0 +1,13 @@
+package com.zoho.Customer;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateProjectTest {
+
+	@Test
+	public void test()
+	{
+		Reporter.log("create the project successfully");
+	}
+}
